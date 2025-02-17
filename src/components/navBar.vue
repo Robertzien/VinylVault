473ee0bfab97fc45b6e62e111803d921b42e5fee
robert <robertziengs03@gmail.com>
2025-02-17 17:28:59 +0100

    Updated navbar, added wishlist button

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -25,6 +25,15 @@
                 </li>
             </ul>
         </div>
+        <div class="wishlist">
+            <button href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="18" height="18" stroke-width="2">
+                <path d="M11.537 19.542l-7.037 -6.97a5 5 0 1 1 7.5 -6.566a5 5 0 0 1 8.212 5.693"></path>
+                <path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5"></path>
+                </svg>
+                Verlanglijst
+            </button>
+        </div>
 	</div>
 </template>
 
@@ -32,6 +41,7 @@
 .container--nav {
     flex-flow: row nowrap;
     align-items: center;
+    justify-content: space-between;
 }
 
 .navbar__logo {
@@ -49,9 +59,22 @@
     list-style: none;
     li {
         margin-right: 2rem;
-        a {
-            text-decoration: none;
+    }
+}
+
+.wishlist {
+    button {
+        align-items: center;
+        display: flex;
+        flex-flow: row nowrap;
+        font-weight: bold;
+        transition: all .2s;
+        svg {
+            margin-right: .5rem;
         }
+    }
+    button:hover {
+        background: #df8032;
     }
 }
 </style>
